3e409d12ebbd671de64de0d380f0d48b6b48104b
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-06 20:29:03 -0300

    Melhorando o layout do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col, Icon, Affix, Form, Button, Input } from 'antd';
+import { Flex, Box } from '@rebass/grid'
 
 class Footer extends React.Component {
     render() {
@@ -7,7 +8,7 @@ class Footer extends React.Component {
         return (
             <Affix offsetBottom={0}>
                 <div className="ps-chat-footer">
-                    <Form layout="inline">
+                    {/* <Form layout="inline">
                         <Row type="flex" align="middle" justify="end">
                             <Col xs={22} sm={22} md={22} lg={22} xl={22}>
                                 <Form.Item>
@@ -23,6 +24,19 @@ class Footer extends React.Component {
                                 </Form.Item>
                             </Col>
                         </Row>
+                    </Form> */}
+                    <Form layout="inline">
+                        <Flex flexDirection={"row"}>
+                            <Box width={4 / 5} alignSelf="flex-start">
+                                {getFieldDecorator('message')(
+                                    <Input style={{padding:"10px"}} placeholder="Write a message..." />
+                                )}
+                            </Box>
+                            <Box width={1 / 5} alignSelf="flex-end">
+                                <Button type="primary" htmlType="submit" icon="up">
+                                </Button>
+                            </Box>
+                        </Flex>
                     </Form>
                 </div>
             </Affix>
