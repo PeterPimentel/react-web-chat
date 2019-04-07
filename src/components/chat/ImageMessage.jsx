60cc179dccd54a33a222a8c86d2b7c243ec62c08
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-07 10:18:41 -0300

    Adicionando o rel

--- src/components/chat/ImageMessage.jsx
+++ src/components/chat/ImageMessage.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function ReceivedMessage({ conversation }) {
     return (
         <div className="ps-chat-image-message">
-            <a href={conversation.image.url} target="_blank">
-                <img src={conversation.image.url}/>
+            <a href={conversation.image.url} target="_blank" rel="noopener noreferrer">
+                <img alt="Imagem enviada pelo usuário" src={conversation.image.url}/>
             </a>
         </div>
     )
