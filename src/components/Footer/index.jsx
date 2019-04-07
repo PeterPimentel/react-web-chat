efef029127b51a698e64a445afa938580883d038
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-07 09:16:21 -0300

    ajustes visuais

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {sendMessage} from '../../reducers/messageReducer';
-import {Affix, Form, Button, Input} from 'antd';
-import {Flex, Box} from '@rebass/grid';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { sendMessage } from '../../reducers/messageReducer';
+import { Affix, Form, Button, Input} from 'antd';
+import { Flex, Box } from '@rebass/grid';
 
 class Footer extends React.Component {
-
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, value) => {
-          if (!err) {
-            console.log('Received values of form: ', value);
-            const message = {
-                type: 'sent',
-                message: value.message,
-                sentAt: '3:08:35 PM',
+            if (!err) {
+                console.log('Received values of form: ', value);
+                const message = {
+                    type: 'sent',
+                    message: value.message,
+                    sentAt: '3:08:35 PM',
+                }
+                this.props.sendMessage(message)
+                this.props.form.resetFields();
             }
-            this.props.sendMessage(message)
-            this.props.form.resetFields();
-          }
         });
     }
 
@@ -30,13 +29,13 @@ class Footer extends React.Component {
                 <div className="ps-chat-footer">
                     <Form layout="inline" onSubmit={this.handleSubmit}>
                         <Flex flexDirection={"row"}>
-                            <Box width={11/12} alignSelf="flex-start">
+                            <Box width={11 / 12} alignSelf="flex-start">
                                 {getFieldDecorator("message")(
-                                    <Input placeholder="Write a message..."/>
+                                    <Input placeholder="Write a message..." />
                                 )}
                             </Box>
-                            <Box width={1/12} alignSelf="flex-end">
-                                <Button htmlType="submit" className="ps-transparent-button ps-chat-footer-button" icon="arrow-right"></Button>
+                            <Box width={1 / 12} alignSelf="flex-end">
+                                <Button htmlType="submit" className="ps-transparent-button ps-footer-button" icon="arrow-right"></Button>
                             </Box>
                         </Flex>
                     </Form>
@@ -45,7 +44,6 @@ class Footer extends React.Component {
         )
     }
 }
-
 const FooterForm = Form.create()(Footer);
 
 const mapDispatchToProps = dispatch =>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,7 +13,7 @@ export default function Header() {
                     <span className="ps-username-name">Botinho</span>
                 </Box>
                 <Box width={1/12} alignSelf="flex-end">
-                    <Button className="ps-transparent-button ps-chat-footer-button" icon="more"></Button>
+                    <Button className="ps-transparent-button ps-footer-button" icon="more"></Button>
                 </Box>
             </Flex>            
         </Affix>
