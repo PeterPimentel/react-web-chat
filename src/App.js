8bea15d6e594cc20150142dccb99b08ee650a092
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-08 21:43:32 -0300

    Inserindo o component scrollBars

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import ChatConatiner from './components/ChatContainer';
 import Footer from './components/Footer';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Header />
-				<ChatConatiner />
-				<Footer/>
+				<Scrollbars autoHide style={{ width: '100vw', height: '100vh' }}>
+					<Header />
+					<ChatConatiner />
+					<Footer />
+				</Scrollbars>
 			</div>
 		);
 	}
