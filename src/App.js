328f12916b576c074e306b437fbdf6f94d50145e
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-11 08:51:50 -0300

    Ajustes no controle da scrollBar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import ChatConatiner from './components/ChatContainer';
 import Footer from './components/Footer';
-import { Scrollbars } from 'react-custom-scrollbars';
 
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Scrollbars autoHide style={{ width: '100vw', height: '100vh' }}>
-					<Header />
-					<ChatConatiner />
-					<Footer />
-				</Scrollbars>
+				<Header />
+				<ChatConatiner />
+				<Footer />
 			</div>
 		);
 	}
--- src/components/ChatContainer/index.jsx
+++ src/components/ChatContainer/index.jsx
@@ -5,30 +5,35 @@ import Message from '../chat/Message';
 import { Flex, Box } from '@rebass/grid';
 import { Button } from 'antd';
 import { sendMessage } from '../../reducers/messageReducer';
-import {convertMessage} from '../../utils/messageUtil';
+import { convertMessage } from '../../utils/messageUtil';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 function ChatContainer(props) {
-    
-    function selectOption(value){
-        props.sendMessage(convertMessage('me',value))
+
+    function selectOption(value) {
+        props.sendMessage(convertMessage('me', value))
     }
 
     const { conversation } = props;
     return (
+        <Scrollbars autoHide style={{ width: '100vw', height: '87vh' }}>
         <div className="ps-chat-container">
             <Flex flexDirection={"column"}>
+
                 {conversation.map((conv, index) => {
                     return (
-                        <Message key={index} conversation={conv}/>
+                        <Message key={index} conversation={conv} />
                     )
                 })}
+
                 <Box className="ps-message-options" alignSelf="center">
-                    <Button onClick={()=>{selectOption("Olá")}}>Olá</Button>
+                    <Button onClick={() => { selectOption("Olá") }}>Olá</Button>
                     <Button>Teste</Button>
                     <Button>Teste</Button>
                 </Box>
             </Flex>
         </div>
+        </Scrollbars>
     )
 }
 
@@ -36,4 +41,4 @@ const mapStateToProps = store => ({
     conversation: store.messageReducer.conversation
 });
 const mapDispatchToProps = dispatch => bindActionCreators({ sendMessage }, dispatch);
-export default connect(mapStateToProps,mapDispatchToProps)(ChatContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ChatContainer);
