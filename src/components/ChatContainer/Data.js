810b05dfe5077cded5796a517c26be1a5d167e95
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-10 22:16:28 -0300

    Ajustes no layout e correção de nomes de variáveis

--- src/components/ChatContainer/Data.js
+++ src/components/ChatContainer/Data.js
@@ -2,38 +2,38 @@ export default [
   {
     'from': 'me',
     'message': 'It is a long established fact',
-    'meAt': '3:08:35 PM',
+    'sentAt': '3:08:35 PM',
   }, {
     'from': 'bot',
     'message': 'I must explain to you how all this mistaken idea of denouncing ',
-    'meAt': '3:10:28 PM',
+    'sentAt': '3:10:28 PM',
   }, {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested',
-    'meAt': '3:11:25 PM',
+    'sentAt': '3:11:25 PM',
   }, {
     'from': 'bot',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:12:36 PM',
+    'sentAt': '3:12:36 PM',
     'image':{
       'url':'https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg'
     }
   }, {
     'from': 'bot',
     'message': 'All the Lorem Ipsum generators on the',
-    'meAt': '3:12:45 PM',
+    'sentAt': '3:12:45 PM',
   }, {
     'from': 'me',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:13:04 PM',
+    'sentAt': '3:13:04 PM',
   }, {
     'from': 'bot',
     'message': 'It is a long established fact',
-    'meAt': '3:13:28 PM',
+    'sentAt': '3:13:28 PM',
   }, {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s',
-    'meAt': '3:15:45 PM',
+    'sentAt': '3:15:45 PM',
     'image':{
       'url':'https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636__340.jpg'
     }
@@ -41,104 +41,104 @@ export default [
   {
     'from': 'me',
     'message': 'English versions from the 1914 translation by H. Rackham',
-    'meAt': '3:03:28 PM',
+    'sentAt': '3:03:28 PM',
   },
   {
     'from': 'bot',
     'message': 'English versions from the 1914 translation by H. Rackham',
-    'meAt': '3:05:47 PM',
+    'sentAt': '3:05:47 PM',
   },
   {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested',
-    'meAt': '3:11:25 PM',
+    'sentAt': '3:11:25 PM',
   }, {
     'from': 'bot',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:12:36 PM',
+    'sentAt': '3:12:36 PM',
   }, {
     'from': 'bot',
     'message': 'All the Lorem Ipsum generators on the',
-    'meAt': '3:12:45 PM',
+    'sentAt': '3:12:45 PM',
   }, {
     'from': 'me',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:13:04 PM',
+    'sentAt': '3:13:04 PM',
   }, {
     'from': 'bot',
     'message': 'It is a long established fact',
-    'meAt': '3:13:28 PM',
+    'sentAt': '3:13:28 PM',
   }, {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s',
-    'meAt': '3:15:45 PM',
+    'sentAt': '3:15:45 PM',
   },
   {
     'from': 'me',
     'message': 'It is a long established fact',
-    'meAt': '3:08:35 PM',
+    'sentAt': '3:08:35 PM',
   }, {
     'from': 'bot',
     'message': 'I must explain to you how all this mistaken idea of denouncing ',
-    'meAt': '3:10:28 PM',
+    'sentAt': '3:10:28 PM',
   }, {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested',
-    'meAt': '3:11:25 PM',
+    'sentAt': '3:11:25 PM',
   }, {
     'from': 'bot',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:12:36 PM',
+    'sentAt': '3:12:36 PM',
   }, {
     'from': 'bot',
     'message': 'All the Lorem Ipsum generators on the',
-    'meAt': '3:12:45 PM',
+    'sentAt': '3:12:45 PM',
   }, {
     'from': 'me',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:13:04 PM',
+    'sentAt': '3:13:04 PM',
   }, {
     'from': 'bot',
     'message': 'It is a long established fact',
-    'meAt': '3:13:28 PM',
+    'sentAt': '3:13:28 PM',
   }, {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s',
-    'meAt': '3:15:45 PM',
+    'sentAt': '3:15:45 PM',
   },
   {
     'from': 'me',
     'message': 'English versions from the 1914 translation by H. Rackham',
-    'meAt': '3:03:28 PM',
+    'sentAt': '3:03:28 PM',
   },
   {
     'from': 'bot',
     'message': 'English versions from the 1914 translation by H. Rackham',
-    'meAt': '3:05:47 PM',
+    'sentAt': '3:05:47 PM',
   },
   {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested',
-    'meAt': '3:11:25 PM',
+    'sentAt': '3:11:25 PM',
   }, {
     'from': 'bot',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:12:36 PM',
+    'sentAt': '3:12:36 PM',
   }, {
     'from': 'bot',
     'message': 'All the Lorem Ipsum generators on the',
-    'meAt': '3:12:45 PM',
+    'sentAt': '3:12:45 PM',
   }, {
     'from': 'me',
     'message': 'There are many variations of passages of ',
-    'meAt': '3:13:04 PM',
+    'sentAt': '3:13:04 PM',
   }, {
     'from': 'bot',
     'message': 'It is a long established fact',
-    'meAt': '3:13:28 PM',
+    'sentAt': '3:13:28 PM',
   }, {
     'from': 'me',
     'message': 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested',
-    'meAt': '3:15:45 PM',
+    'sentAt': '3:15:45 PM',
   }
 ]
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import {Avatar, Button, Affix } from 'antd';
-import {Flex, Box} from '@rebass/grid';
+import { Avatar, Button } from 'antd';
+import { Flex, Box } from '@rebass/grid';
 
 export default function Header() {
     return (
-        <Affix>
-            <div className="ps-chat-header">
-                <Flex flexDirection={"row"} alignItems="center">
-                    <Box width="10vw">
-                        <Avatar src="http://icons.iconarchive.com/icons/diversity-avatars/avatars/1024/robot-03-icon.png"/>
-                    </Box>
-                    <Box width="80vw">
-                        <span className="ps-bot-name">Botinho</span>
-                    </Box>
-                    <Box width="10vw" alignSelf="flex-end">
-                        <Button className="ps-transparent-button ps-footer-button" icon="more"></Button>
-                    </Box>
-                </Flex>
-            </div>
-        </Affix>
+        <div className="ps-chat-header">
+            <Flex flexDirection={"row"} alignItems="center">
+                <Box width="10vw">
+                    <Avatar src="http://icons.iconarchive.com/icons/diversity-avatars/avatars/1024/robot-03-icon.png" />
+                </Box>
+                <Box width="80vw">
+                    <span className="ps-bot-name">Botinho</span>
+                </Box>
+                <Box width="10vw" alignSelf="flex-end">
+                    <Button className="ps-transparent-button ps-footer-button" icon="more"></Button>
+                </Box>
+            </Flex>
+        </div>
     )
 }
