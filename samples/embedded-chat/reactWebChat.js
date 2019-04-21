91466df451ed2d3aeb233a670ffa66d17a1592e0
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-21 11:35:23 -0300

    Adicionado página de loading (skeleton) do chat

--- samples/embedded-chat/reactWebChat.js
+++ samples/embedded-chat/reactWebChat.js
@@ -1,18 +1,14 @@
-window.onload = function () {
-    // First we check if you support touch, otherwise it's click:
-    let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
-    const psButtonChat = document.querySelector('.ps-button-circle');
-    const psIframeChat = document.querySelector('.ps-web-chat-iframe');
+const psIframeChat = document.querySelector('.ps-web-chat-iframe');
+const psButtonChat = document.querySelector('.ps-button-circle');
 
+psIframeChat.onload = () => {
+    console.log('iframe is completely loaded');
+}
+
+window.onload = () => {
     let iframeState = false;
-    psIframeChat.style.display = 'none';
-
-    psButtonChat.addEventListener(touchEvent, () => {
-        if(iframeState){
-            psIframeChat.style.display = 'none'
-        }else{
-            psIframeChat.style.display = 'initial'
-        }
+    psButtonChat.addEventListener('click', () => {
+        psIframeChat.style.display = iframeState ? 'none' : 'inherit'
         iframeState = !iframeState;
     });
 }
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Chat from './components/pages/Chat';
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import Chat from './components/pages/Chat';
+import SkeletonChat from './components/pages/SkeletonChat';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Router>
-					<Route path="/" component={Chat} />
+					<Route exact path="/" component={Chat} />
+					<Route path="/skeleton" component={SkeletonChat} />
 				</Router>
 			</div>
 		);
