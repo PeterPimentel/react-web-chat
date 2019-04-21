10fc1d998339335bf0247531279b2cbc61d47a86
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-21 15:03:33 -0300

    Movido declaração do skeleton para o script de embarcar o chat

--- samples/embedded-chat/reactWebChat.js
+++ samples/embedded-chat/reactWebChat.js
@@ -1,14 +1,42 @@
 const psIframeChat = document.querySelector('.ps-web-chat-iframe');
 const psButtonChat = document.querySelector('.ps-button-circle');
-
+const psChatSkeleton = document.querySelector('#ps-chat-skeleton');
+let iframeLoaded = false;
+let iframeState = false;
+const iframeSkeleton = `
+    <div>
+        <div class="ps-skeleton-header">
+            <div class="ps-skeleton-avatar"></div>
+            <div class="ps-skeleton-name"></div>
+        </div>
+        <div class="ps-skeleton-content color-change-2x">
+        </div>
+        <div class="ps-skeleton-footer">
+        </div>
+    </div>
+`
 psIframeChat.onload = () => {
+    setInterval(() => {
+        psChatSkeleton.style.display = 'none';
+        if(iframeState){
+            psIframeChat.style.display = 'inherit';
+        }
+        iframeLoaded = true;
+        console.log('INtervarl iframe is loaded');    
+    }, 6000);
     console.log('iframe is completely loaded');
 }
 
 window.onload = () => {
-    let iframeState = false;
+    if(psChatSkeleton){
+        psChatSkeleton.innerHTML = iframeSkeleton;
+    }
     psButtonChat.addEventListener('click', () => {
-        psIframeChat.style.display = iframeState ? 'none' : 'inherit'
+        if(iframeLoaded){
+            psIframeChat.style.display = iframeState ? 'none' : 'inherit';
+        }else{
+            psChatSkeleton.style.display = iframeState ? 'none' : 'inherit'
+        }
         iframeState = !iframeState;
     });
 }
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Chat from './components/pages/Chat';
-import SkeletonChat from './components/pages/SkeletonChat';
 
 class App extends Component {
 	render() {
@@ -9,7 +8,6 @@ class App extends Component {
 			<div className="App">
 				<Router>
 					<Route exact path="/" component={Chat} />
-					<Route path="/skeleton" component={SkeletonChat} />
 				</Router>
 			</div>
 		);
