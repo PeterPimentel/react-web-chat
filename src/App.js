cd4c5ba7970a7b382ebf0edbf35fe82c2975df1b
Peter Vinicius Pimentel <petervpimentel@gmail.com>
2019-04-27 22:39:01 -0300

    Criando tela de login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Chat from './components/pages/Chat';
+import Login from './components/pages/Login';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Router>
-					<Route exact path="/" component={Chat} />
+					<Route exact path="/" component={Login} />
 				</Router>
 			</div>
 		);
